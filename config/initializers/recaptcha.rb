b1926f1e9502bcf5efc94a177b940bfbe3f305bf
Igor Onischenko <optimum.flaminis@gmail.com>
2016-08-15 10:01:46 +0300

    minor changes and hotfix

---
+++ config/initializers/recaptcha.rb
@@ -0,0 +1,5 @@
+# config/initializers/recaptcha.rb
+Recaptcha.configure do |config|
+  config.public_key  = '6LdumycTAAAAAFt0-ljb2vOOrsuTHBnD9qUnkJuZ'
+  config.private_key = '6LdumycTAAAAAJlVzzE1kjWmf54rtqZhAB8spZRH'
+end
