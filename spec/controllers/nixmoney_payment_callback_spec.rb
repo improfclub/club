3379cd5b79fb411c2b59d12a434740983dcd3022
alexandr <sania.ace@gmail.com>
2016-09-18 18:35:04 +0200

    order number added

--- spec/controllers/nixmoney_payment_callback_spec.rb
+++ spec/controllers/nixmoney_payment_callback_spec.rb
@@ -8,14 +8,14 @@ RSpec.describe FinanceApiController, type: :controller do
       expect { 
       	post :responce_status, :id => "nixmoney", 
       	"user_id" => 2, 
-      	'PAYMENT_ID' => 'NULL',
+      	'PAYMENT_ID' => '1',
   	  	'PAYEE_ACCOUNT' => 'U52137923986425',
   	  	'PAYMENT_AMOUNT' => '1.00',
   	  	'PAYMENT_UNITS' => 'USD',
-  	  	'PAYMENT_BATCH_NUM' => '607859',
+  	  	'PAYMENT_BATCH_NUM' => '607873',
   	  	'PAYER_ACCOUNT' => 'U33537306942179',
-  	  	'V2_HASH' => 'F56D5CB00BA5F3CE1A6DCC89C0394A88',
-  	  	'TIMESTAMPGMT' => '1474210833'
+  	  	'V2_HASH' => 'F645AEDA110F9A0DBF7DF538423F0661',
+  	  	'TIMESTAMPGMT' => '1474215852'
 		}.to  change { Payment.count }
       # expect(wallet.main_balance).to   eq(balance_prev + mock_params['ac_amount'].to_f)
       # expect(response).to have_http_status(200)
